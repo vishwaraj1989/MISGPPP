bd475b56caa140977c8124d8e9f1387b82e2e0d6
NearMeWebsite <nearmewebsites@gmail.com>
2024-08-26 10:27:00 +0530

    Login IP Change

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,85 @@
 
 
+// import React, { useState, useRef, useEffect } from 'react';
+// import axios from 'axios';
+// import { useNavigate } from 'react-router-dom';
+// import './Login.css'; // Import the updated CSS file
+
+// const Login = () => {
+//   const [formData, setFormData] = useState({
+//     email: '',
+//     password: ''
+//   });
+
+//   const [message, setMessage] = useState('');
+//   const navigate = useNavigate();
+//   const emailRef = useRef(null); // Create a ref for the email input field
+
+//   useEffect(() => {
+//     // Focus on the email input field when the component mounts
+//     if (emailRef.current) {
+//       emailRef.current.focus();
+//     }
+//   }, []);
+
+//   const handleChange = (e) => {
+//     setFormData({
+//       ...formData,
+//       [e.target.name]: e.target.value
+//     });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await axios.post('http://localhost:5000/api/authRoutes/login', formData);
+//       setMessage(response.data.message);
+//       localStorage.setItem('token', response.data.token);
+//       navigate('/Dashboard');
+//     } catch (error) {
+//       setMessage(error.response?.data?.message || 'Something went wrong!');
+//     }
+//   };
+
+//   // const handleSignupClick = () => {
+//   //   navigate('/signup');
+//   // };
+
+//   return (
+//     <div className="login-container">
+//       <div className="login-box">
+//         <h2>Login</h2>
+//         <form onSubmit={handleSubmit}>
+//           <input
+//             type="email"
+//             name="email"
+//             placeholder="Email"
+//             value={formData.email}
+//             onChange={handleChange}
+//             required
+//             ref={emailRef} // Attach the ref to the email input field
+//           />
+//           <input
+//             type="password"
+//             name="password"
+//             placeholder="Password"
+//             value={formData.password}
+//             onChange={handleChange}
+//             required
+//           />
+//           <button type="submit" className="login-button">Login</button>
+//         </form>
+//         {message && <p>{message}</p>}
+//         {/* <button onClick={handleSignupClick} className="signup-button">Signup</button> */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+// --------- Post IP Address Change ---------------
+
 import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -32,7 +112,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/authRoutes/login', formData);
+      const response = await axios.post('http://76.76.21.9:5000/api/authRoutes/login', formData);
       setMessage(response.data.message);
       localStorage.setItem('token', response.data.token);
       navigate('/Dashboard');
@@ -40,10 +120,6 @@ const Login = () => {
       setMessage(error.response?.data?.message || 'Something went wrong!');
     }
   };
-
-  // const handleSignupClick = () => {
-  //   navigate('/signup');
-  // };
 
   return (
     <div className="login-container">
@@ -70,7 +146,6 @@ const Login = () => {
           <button type="submit" className="login-button">Login</button>
         </form>
         {message && <p>{message}</p>}
-        {/* <button onClick={handleSignupClick} className="signup-button">Signup</button> */}
       </div>
     </div>
   );
